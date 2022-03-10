9de3e639c2a00dc943e60cb01f7ecc9bf46ba064
Julio Junior <juniorknx@hotmail.com>
2022-03-10 00:59:45 -0300

    Adicionando detalhes do repositorio

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,5 +1,36 @@
-export function Repositorio(){
+import { Container } from "./styles";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
+
+export function Repositorio() {
+    const [repository, setRepository] = useState({});
+    const [issues, setIssues] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const { repositorio } = useParams();
+
+    useEffect(() => {
+        async function load() {
+            const [repositorioData, issuesData] = await Promise.all([
+                api.get(`/repos/${repositorio}`),
+                api.get(`/repos/${repositorio}/issues`, {
+                    params: {
+                        state: 'open',
+                        per_page: 5,
+                    }
+                }),
+            ])
+            setRepository(repositorioData.data);
+            setIssues(issuesData.data);
+            setLoading(false);
+        }
+
+        load();
+    }, [repositorio])
+
     return (
-        <h1>Repo Page</h1>
+        <Container>
+
+        </Container>
     )
 }
